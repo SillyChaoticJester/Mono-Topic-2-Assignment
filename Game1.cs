642e800546a2af965f0d9d035225a8b9d6d4c10f
Miracle McMahon <339663114@GoTVDSB.ca>
2025-04-02 13:48:03 -0400

    Add project files.

---
+++ Game1.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+
+namespace Mono_Topic_2_Assignment
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        Rectangle window;
+        Random generator;
+        Texture2D spaceBackground;
+        List<Texture2D> textures;
+        List<Texture2D> planetTextures;
+        List<Rectangle> planetRects;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            window = new Rectangle(0, 0, _graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferHeight);
+            generator = new Random();
+            textures = new List<Texture2D>();
+            planetTextures = new List<Texture2D>();
+            planetRects = new List<Rectangle>();
+            for (int i = 0; i < 30;  i++)
+            {
+                planetRects.Add
+                    (
+                    new Rectangle(generator.Next(window.Width - 25), generator.Next(window.Height - 25), generator.Next(25, 101), generator.Next(25, 101))
+                    );
+            }
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new Mono_Topic_2_Assignment.Game1();
+game.Run();
